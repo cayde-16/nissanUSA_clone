31e4ca00b23c43708e0adceae4bfb787866472ee
Cade Alexander Heck <cadeheck16@gmail.com>
2022-11-17 16:30:58 -0500

    worked on navbar functionality

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 const webSite = 'https://www.nissanusa.com/vehicles/sports-cars/gt-r.html' 
 console.log(webSite)
+
+
+const mobileMenu = document.querySelector('.close');
+const navLinks = document.querySelector('.navbar-links');
+const openList = document.querySelector('.open-list');
+
+
+openList.addEventListener("click", () => {
+    navLinks.classList.toggle('show-link');
+    openList.classList.toggle('toggle-open-list')
+    mobileMenu.classList.toggle('toggle-close')
+
+})
+
+mobileMenu.addEventListener('click', () =>{
+    mobileMenu.classList.toggle('toggle-close');
+    openList.classList.toggle('toggle-open-list');
+    navLinks.classList.toggle('show-link');
+})
