42210023e58760d7adfe2f3947b3c0100c0a0f07
Cade Alexander Heck <cadeheck16@gmail.com>
2022-11-24 20:21:52 -0500

    remodeled HTML structure

--- index.js
+++ index.js
@@ -2,20 +2,37 @@ const webSite = 'https://www.nissanusa.com/vehicles/sports-cars/gt-r.html'
 console.log(webSite)
 
 
-const mobileMenu = document.querySelector('.close');
-const navLinks = document.querySelector('.navbar-links');
-const openList = document.querySelector('.open-list');
+const hamburgerIcon = document.querySelector('.hamburger--icon');
+const navbarMobileLinks = document.querySelector('.navlinks--mobile');
+const closeIcon = document.querySelector('.close--icon');
 
-
-openList.addEventListener("click", () => {
-    navLinks.classList.toggle('show-link');
-    openList.classList.toggle('toggle-open-list')
-    mobileMenu.classList.toggle('toggle-close')
-
+hamburgerIcon.addEventListener("click", () => {
+    navbarMobileLinks.classList.toggle('show--mobile--links');
+    hamburgerIcon.classList.toggle('hide--hamburger--icon');
+    closeIcon.classList.toggle('show--close--icon')
 })
 
-mobileMenu.addEventListener('click', () =>{
-    mobileMenu.classList.toggle('toggle-close');
-    openList.classList.toggle('toggle-open-list');
-    navLinks.classList.toggle('show-link');
+closeIcon.addEventListener('click', () => {
+    navbarMobileLinks.classList.toggle('show--mobile--links');
+    hamburgerIcon.classList.toggle('hide--hamburger--icon');
+    closeIcon.classList.toggle('show--close--icon')
 })
+
+
+// const mobileMenu = document.querySelector('.close');
+// const navLinks = document.querySelector('.navbar-links');
+// const openList = document.querySelector('.open-list');
+
+
+// openList.addEventListener("click", () => {
+//     navLinks.classList.toggle('show-link');
+//     openList.classList.toggle('toggle-open-list')
+//     mobileMenu.classList.toggle('toggle-close')
+
+// })
+
+// mobileMenu.addEventListener('click', () =>{
+//     mobileMenu.classList.toggle('toggle-close');
+//     openList.classList.toggle('toggle-open-list');
+//     navLinks.classList.toggle('show-link');
+// })
