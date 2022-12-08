57bc8d057012636edcc0ab214899b42be5c09c48
Cade Alexander Heck <cadeheck16@gmail.com>
2022-12-08 17:22:39 -0500

    added swiper feature

---
+++ swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    centeredSlides: false,
+    allowSlideNext:true,
+    effect: 'cube',
+    longSwipes:false,
+    grabCursor:true,
+    edgeSwipeDetection:'prevent',
+    loop: true,
+
+
+    // If we need pagination
+    // pagination: {
+    //     el: '.swiper-pagination',
+    // },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    
+});
